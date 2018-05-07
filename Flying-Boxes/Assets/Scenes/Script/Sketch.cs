3d73906804377cc743cfa3ce4fb174876dbf3282
Paige Sun <paige.sun.dev@gmail.com>
2018-05-06 23:02:56 -0400

    Create two sets of cubes -- both are distributed linearly (equal spacing between positions).

--- Flying-Boxes/Assets/Scenes/Script/Sketch.cs
+++ Flying-Boxes/Assets/Scenes/Script/Sketch.cs
@@ -13,6 +13,7 @@ public class Sketch : MonoBehaviour {
         // The distance between the position of the first and last cube
         int totalDistance = 5;
 
+        // LINEAR DISTRIBUTION
         for (int i = 0; i < totalCubes; i++) {
             
             float percentage = i / (float)totalCubes; // increases as i increases
@@ -20,14 +21,33 @@ public class Sketch : MonoBehaviour {
 
             // Positions
             float x = percentage * totalDistance;
-            float y = 5.0f;
+            float y = 6.0f;
             float z = 0.0f;
 
             // Create a new cube, set its initial position, rotation, scale, and speed of rotation.
             GameObject newCube = Instantiate(spinCube, new Vector3(x, y, z), Quaternion.identity);
             CubeScript cubeScript = newCube.GetComponent<CubeScript>();
             cubeScript.SetScale(inversePercentage);
-            cubeScript.rotateAroundSpeed = percentage;
+            cubeScript.rotateAroundSpeed = 0;
+        }
+
+        // SIN DISTRIBUTION
+        for (int i = 0; i < totalCubes; i++)
+        {
+
+            float percentage = i / (float)totalCubes; // increases as i increases
+            float inversePercentage = 1.0f - percentage; // decreases as i increases
+
+            // Positions
+            float x = percentage * totalDistance;
+            float y = 3.0f;
+            float z = 0.0f;
+
+            // Create a new cube, set its initial position, rotation, scale, and speed of rotation.
+            GameObject newCube = Instantiate(spinCube, new Vector3(x, y, z), Quaternion.identity);
+            CubeScript cubeScript = newCube.GetComponent<CubeScript>();
+            cubeScript.SetScale(inversePercentage);
+            cubeScript.rotateAroundSpeed = 0;
         }
 	}
 	
