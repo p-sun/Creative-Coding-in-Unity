89c0edcbea4b8fe4e81235ad751fec43ad6113d6
Paige Sun <paige.sun.dev@gmail.com>
2018-05-06 21:10:57 -0400

    Create a new script to rotate the cube, and set its initial position and scale.

---
+++ Flying-Boxes/Assets/Scenes/Script/CubeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector3(0, 5, 0);
+        this.transform.localScale = new Vector3(2, 2, 2);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(.1f, .1f, .1f);
+	}
+}
