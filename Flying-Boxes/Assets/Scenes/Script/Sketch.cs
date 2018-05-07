222c60a1775162fc57bd927c957efee0b39d5cd0
Paige Sun <paige.sun.dev@gmail.com>
2018-05-06 22:29:55 -0400

    Create a empty object holding "Sketch.cs", which instantiates a spin cube.

---
+++ Flying-Boxes/Assets/Scenes/Script/Sketch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sketch : MonoBehaviour {
+
+    public GameObject spinCube;
+
+	void Start () {
+
+        // Create a new cube, set its initial position, rotation, and scale.
+        GameObject newCube = Instantiate(spinCube, new Vector3(3, 5, 0), Quaternion.identity);
+
+        Instantiate(spinCube, new Vector3(3, 5, 0), Quaternion.identity);
+        newCube.GetComponent<CubeScript>().SetScale(3.0f);
+	}
+	
+	void Update () {
+		
+	}
+}
