95bf538b99e0b6eec9fe7dccf0d5feade6519e88
Paige Sun <paige.sun.dev@gmail.com>
2018-05-06 21:47:37 -0400

    Randomize spinspeed on start.

--- Flying-Boxes/Assets/Scenes/Script/CubeScript.cs
+++ Flying-Boxes/Assets/Scenes/Script/CubeScript.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour {
 
+    Vector3 spinSpeed = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
-        this.transform.position = new Vector3(0, 5, 0);
-        this.transform.localScale = new Vector3(2, 2, 2);		
+        
+        this.transform.position = new Vector3(0f, 5f, 0f);
+        this.transform.localScale = new Vector3(2f, 2f, 2f);
+
+        spinSpeed = new Vector3(Random.value, Random.value, Random.value);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Rotate(.1f, .1f, .1f);
+        
+        this.transform.Rotate(spinSpeed);
 	}
 }
